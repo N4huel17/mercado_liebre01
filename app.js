e73338d8051918de7bd9007f2e74447bfc2e3cce
N4huel17 <jesusdsm17@gmail.com>
2023-11-09 20:16:08 -0300

    correcciones terminadas

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app= express();
 const PORT= 3030;
 app.use(express.static(path.join(__dirname,'public')))
 app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'views','home.html')))
-
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
+app.get('/profile',(req,res) => res.sendFile(path.join(__dirname,'views','profile.html')))
 
 
 app.listen(PORT,()=> console.log('Servidor corriendo en el puerto http://localhost:'+PORT));
